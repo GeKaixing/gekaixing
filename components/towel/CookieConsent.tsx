f425ffb0185d0fcda2c293e2a4721763717bf06d
GeKaixing <x2890901420@Gmail.com>
2025-08-10 13:47:32 +0800

    1.0.8()

--- components/towel/CookieConsent.tsx
+++ components/towel/CookieConsent.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, CardContent, CardFooter } from '../ui/card'
 import Link from 'next/link'
 import { Button } from '../ui/button'
 
 export default function CookieConsent() {
-    
+    const [cookieConsent, setCookieConsent] = useState(false)
+    useEffect(() => {
+        const data = localStorage.getItem('CookieConsent')
+        if (data) {
+            setCookieConsent(true)
+        } else {
+            setCookieConsent(false)
+        }
+    }, [])
     return (
-        <Card className='bg-black border-black fixed bottom-6 
+        cookieConsent && <Card className='bg-black border-black fixed bottom-6 
             sm:max-w-[400px] sm:right-6 '>
             <CardContent className='bg-black text-white'>
                 This website uses cookies to ensure you get the best experience on our website.
@@ -15,7 +23,11 @@ export default function CookieConsent() {
                 </Link>
             </CardContent>
             <CardFooter>
-                <Button  className='h-9 bg-white text-black hover:bg-gray-500 hover:text-white '>GOT IT</Button>
+                <Button className='h-9 bg-white text-black hover:bg-gray-500 hover:text-white '
+                    onClick={() => {
+                        localStorage.setItem('CookieConsent','true')
+                    }}
+                >GOT IT</Button>
             </CardFooter>
         </Card>
     )
