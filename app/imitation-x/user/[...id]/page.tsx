69da06f8c9e7631c502b84ea2bec2edb55c3e2bf
开星 <x2890901420@gmail.com>
2026-02-15 10:30:38 +0800

    style: 优化用户个人页面布局

--- app/imitation-x/user/[...id]/page.tsx
+++ app/imitation-x/user/[...id]/page.tsx
@@ -29,8 +29,8 @@ export default function Page() {
         })
     }, [])
     return (
-        <div>
-            <div className='h-8'> <ArrowLeftBack></ArrowLeftBack></div>
+        <div >
+            <div className='h-16'> <ArrowLeftBack></ArrowLeftBack></div>
             <div className='bg-gray-400 w-full h-[200px] relative'>
                 {user_background_image && <Image
                     src={user_background_image}
@@ -39,66 +39,69 @@ export default function Page() {
                     priority
                     className='object-cover w-full'  ></Image>}
             </div>
-            <div>
-                <Avatar className="size-36 absolute  -translate-y-1/2 ">
-
-                    <AvatarImage src={user_avatar} />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-            </div>
-            <div className='flex'>
-                <div className='w-full'></div>
-                <UserEditDialog></UserEditDialog>
-            </div>
+            <div className='px-4'>
 
 
-            <div className='w-full h-10'></div>
-            <div className='font-bold text-2xl mb-2'>{name}</div>
-            {brief_introduction ? <div className='text-sm'>{brief_introduction}</div> : <div className='text-sm'>{"还没有介绍自己"}</div>}
-            <div className='w-full h-5'></div>
-            <Tabs defaultValue="post" className="w-full">
-                <TabsList className='w-full'>
-                    <TabsTrigger value="post">帖子</TabsTrigger>
-                    <TabsTrigger value="reply">回复</TabsTrigger>
-                    {/* <TabsTrigger value="article">文章</TabsTrigger>
+                <div>
+                    <Avatar className="size-36 absolute  -translate-y-1/2 ">
+
+                        <AvatarImage src={user_avatar} />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                </div>
+                <div className='flex'>
+                    <div className='w-full'></div>
+                    <UserEditDialog></UserEditDialog>
+                </div>
+
+
+                <div className='w-full h-10'></div>
+                <div className='font-bold text-2xl mb-2'>{name}</div>
+                {brief_introduction ? <div className='text-sm'>{brief_introduction}</div> : <div className='text-sm'>{"还没有介绍自己"}</div>}
+                <div className='w-full h-5'></div>
+                <Tabs defaultValue="post" className="w-full">
+                    <TabsList className='w-full'>
+                        <TabsTrigger value="post">帖子</TabsTrigger>
+                        <TabsTrigger value="reply">回复</TabsTrigger>
+                        {/* <TabsTrigger value="article">文章</TabsTrigger>
                     <TabsTrigger value="media">媒体</TabsTrigger>
                     <TabsTrigger value="like">喜欢</TabsTrigger> */}
-                </TabsList>
-                <TabsContent value="post" className='flex flex-col gap-6'>
-                    {data.map((items: {
-                        id: string,
-                        user_id: string,
-                        user_name: string,
-                        user_email: string,
-                        user_avatar: string,
-                        content: string
-                        like: number,
-                        star: number,
-                        reply_count: number,
-                        share: number
-                    }) => (
-                        <PostCard
-                            key={items.id}
-                            id={items.id}
-                            user_id={items.user_id}
-                            user_name={items.user_name}
-                            user_email={items.user_email}
-                            user_avatar={items.user_avatar}
-                            content={items.content}
-                            like={items.like}
-                            star={items.star}
-                            reply={items.reply_count}
-                            share={items.share}
-                        />
-                    ))}
-                </TabsContent>
-                <TabsContent value="reply" className='flex flex-col gap-6'>    <UserReplyCard /></TabsContent>
-                {/* <TabsContent value="article" className='flex flex-col gap-6'>    <PostCard></PostCard></TabsContent>
+                    </TabsList>
+                    <TabsContent value="post" className='flex flex-col gap-6'>
+                        {data.map((items: {
+                            id: string,
+                            user_id: string,
+                            user_name: string,
+                            user_email: string,
+                            user_avatar: string,
+                            content: string
+                            like: number,
+                            star: number,
+                            reply_count: number,
+                            share: number
+                        }) => (
+                            <PostCard
+                                key={items.id}
+                                id={items.id}
+                                user_id={items.user_id}
+                                user_name={items.user_name}
+                                user_email={items.user_email}
+                                user_avatar={items.user_avatar}
+                                content={items.content}
+                                like={items.like}
+                                star={items.star}
+                                reply={items.reply_count}
+                                share={items.share}
+                            />
+                        ))}
+                    </TabsContent>
+                    <TabsContent value="reply" className='flex flex-col gap-6'>    <UserReplyCard /></TabsContent>
+                    {/* <TabsContent value="article" className='flex flex-col gap-6'>    <PostCard></PostCard></TabsContent>
                 <TabsContent value="media" className='flex flex-col gap-6'>    <PostCard></PostCard></TabsContent>
                 <TabsContent value="like" className='flex flex-col gap-6'>    <PostCard></PostCard></TabsContent> */}
-            </Tabs>
+                </Tabs>
+            </div>
         </div>
-
     )
 }
 
